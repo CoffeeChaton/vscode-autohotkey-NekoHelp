c93976f2f4a703e747f895fd43e014648285a120
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2023-03-20 21:32:28 +0800

    feat: `%i%___%j% = str` , use syntax-highlight

--- src/provider/SemanticTokensProvider/legacyAssignmentHighlight.ts
+++ src/provider/SemanticTokensProvider/legacyAssignmentHighlight.ts
@@ -36,9 +36,10 @@ export function legacyAssignmentHighlight(DocStrMap: TTokenStream): TSemanticTok
         if (!detail.includes(EDetail.inSkipSign2)) continue;
 
         if (
-            (/^[ \t]*\w+[ \t]*=(?!=)/u).test(textRaw)
-            || (/^[ \t]*\w+%\w+%[ \t]*=(?!=)/u).test(textRaw)
-            || (/^[ \t]*%\w+%\w+[ \t]*=(?!=)/u).test(textRaw)
+            (/^[ \t]*\w+[ \t]*=(?!=)/u).test(textRaw) // \w+
+            || (/^[ \t]*\w+%\w+%[ \t]*=(?!=)/u).test(textRaw) // Arr_%i%
+            || (/^[ \t]*%\w+%\w+[ \t]*=(?!=)/u).test(textRaw) // %I%_Arr
+            || (/^[ \t]*%\w+%\w+%\w+%[ \t]*=(?!=)/u).test(textRaw) // %I%_Arr_%j%
         ) {
             continue; // Simple case, taken over by syntax-highlight
         }
